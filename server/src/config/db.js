f6033d3204cb73bf7085b5de1e0da0540d42ebfe
LƯU MỸ NHẬT HUY <huylmnse181744@fpt.edu.vn>
2026-05-18 15:25:28 +0700

    update database

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const env = require("./env");
 
 const connectDB = async () => {
-  await mongoose.connect(env.mongoUri);
+  try {
+    await mongoose.connect(env.mongoUri);
+    console.log("Connected to MongoDB successfully");
+  } catch(error) {
+    console.log("MongoDB connection error:", error);
+  }
 };
 
 module.exports = connectDB;
