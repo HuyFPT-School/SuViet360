8aed1a24df8a78bdcd5e4a32c8be2dd8b1308a87
QuangNhat121 <nhat9983@gmail.com>
2026-05-30 08:09:21 +0700

    remove light-dark

--- client/components/providers.tsx
+++ client/components/providers.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { PropsWithChildren, useEffect, useMemo } from "react";
+import { PropsWithChildren, useMemo } from "react";
 import { Provider } from "react-redux";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { makeStore } from "@/store";
-import { setTheme, type ThemeMode } from "@/store/features/uiSlice";
 
 const GOOGLE_CLIENT_ID =
   process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||
@@ -12,26 +11,6 @@ const GOOGLE_CLIENT_ID =
 
 export default function Providers({ children }: PropsWithChildren) {
   const store = useMemo(() => makeStore(), []);
-  useEffect(() => {
-    const stored = window.localStorage.getItem("sv-theme");
-    if (stored === "light" || stored === "dark") {
-      store.dispatch(setTheme(stored as ThemeMode));
-    }
-
-    const applyTheme = () => {
-      const nextTheme = store.getState().ui.theme;
-      document.documentElement.dataset.theme = nextTheme;
-      window.localStorage.setItem("sv-theme", nextTheme);
-    };
-
-    applyTheme();
-
-    const unsubscribe = store.subscribe(() => {
-      applyTheme();
-    });
-
-    return unsubscribe;
-  }, [store]);
 
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
