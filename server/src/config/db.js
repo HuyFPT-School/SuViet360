cf0c793ad8708976af53654d20d331b6645090aa
VuVanDuc <vuvanduc3012004@gmail.com>
2026-06-12 00:24:40 +0700

    xóa dns 8888

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-const dns = require('dns');
 
-// Fix querySrv ECONNREFUSED by forcing Google DNS
-dns.setServers(['8.8.8.8']);
 
 
 const env = require("./env");
